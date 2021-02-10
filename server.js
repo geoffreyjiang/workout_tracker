9fb60041658e8f8bb96c93c5e6d64a0c8cdbe94f
Geoffrey Jiang <geoffreychjiang@gmail.com>
2021-02-10 11:56:29 -0800

    moved some folders, added models folder w workoutjs

--- server.js
+++ server.js
@@ -2,13 +2,13 @@ const express = require("express");
 const mongoose = require("mongoose");
 const morgan = require("morgan");
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8000;
 
 const app = express();
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-app.listen(8080, () => {
+app.listen(8000, () => {
   console.log(`App running on: http://localhost:${PORT}`);
 });
