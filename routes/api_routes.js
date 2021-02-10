582c360abd57e10ee5e755def2a8341e7d0189a9
Geoffrey Jiang <geoffreychjiang@gmail.com>
2021-02-10 12:59:00 -0800

    added post

--- routes/api_routes.js
+++ routes/api_routes.js
@@ -1,13 +1,24 @@
 const db = require("../models/workout");
 
 module.exports = (app) => {
-    app.get("/api/workouts", (req, res) => {
-      db.Workout.find({})
-  
-        .then((dbWorkout) => {
-          res.json(dbWorkout);
-        })
-        .catch((err) => {
-          res.json(err);
-        });
-    });
+  app.get("/api/workouts", (req, res) => {
+    db.Workout.find({})
+
+      .then((dbWorkout) => {
+        res.json(dbWorkout);
+      })
+      .catch((err) => {
+        res.json(err);
+      });
+  });
+
+  app.post("/api/workouts", (req, res) => {
+    db.Workout.create({})
+      .then((dbWorkout) => {
+        res.json(dbWorkout);
+      })
+      .catch((err) => {
+        res.json(err);
+      });
+  });
+};
